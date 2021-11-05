7a4d3b413c99469b51d790decee4be19fc4a79b0
Hridoy <hridoykhondokar01@gmail.con.com>
2021-11-05 12:25:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cart from './component/Cart/Cart';
+import Shop from './component/Shop/shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Cart></Cart>
+      <Shop></Shop>
+
+    </div>
+  );
+}
+
+export default App;
